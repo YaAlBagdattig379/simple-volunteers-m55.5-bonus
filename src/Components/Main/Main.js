36f43602e9847d6cd9cc94ae0cc60237ab6f1b1f
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-05 03:36:13 +0530

    v-55-5-5 last commit

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,17 +1,42 @@
-// import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect} from 'react';
 import useVolunteers from '../../Hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import Volunteers from '../Volunteers/Volunteers';
 import './Main.css'
 
 const Main = () => {
-    const [volunteers,setVolunteers] = useVolunteers();
+    // const [volunteers,setVolunteers] = useVolunteers();
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    useEffect( () =>{
+        console.log('inside dependency !')
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data =>{
+            const match = data.filter(d => d.title.includes(searchText));
+            setSearchResults(match);
+        })
+    },[searchText])
+    const handleSearchChange = event =>{
+        setSearchText(event.target.value);
+    }
+    /*
+      const handleSearchChange2 = event =>{
+        const searchText = event.target.value;
+        const match = volunteers.filter(v => v.title.includes(searchText));
+        setSearchResults(match);
+    }
+    */
     return (
         <div>
-            <h3>volunteers activies : {volunteers.length}</h3>
+            <h3>volunteers activies : {searchResults.length}</h3>
+            <div style={{'margin': '20px','padding':'5px'}}>
+                  <input onChange={handleSearchChange} type="text"placeholder='search'style={{'padding':'5px','borderRadius'
+                  :'7px'}} />
+            </div>
             <div className="activity-container">
               {
-                volunteers.map(activity => <Activity
+                searchResults.map(activity => <Activity
                      key={activity._id} 
                      activity = {activity}
                 ></Activity>)
--- src/Components/Volunteers/Volunteers.js
+++ src/Components/Volunteers/Volunteers.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useState} from 'react';
 import useVolunteers from '../../Hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 
 const Volunteers = () => {
-    const [volunteers, setVolunteers] = useVolunteers();
+    // const [volunteers, setVolunteers] = useVolunteers();
     const [searchResults, setSearchResults] = useState([]);
+    const [searchText, setSearchText] = useState('');
+
+    useEffect( () =>{
+        console.log('inside dependency !')
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data =>{
+            const match = data.filter(d => d.title.includes(searchText));
+            setSearchResults(match);
+        })
+    },[searchText])
     const handleSearchChange = event =>{
-        const searchText = event.target.value;
-        const match = volunteers.filter(v => v.title.includes(searchText));
-        setSearchResults(match);
+        setSearchText(event.target.value);
     }
     return (
         <div>
@@ -21,6 +30,7 @@ const Volunteers = () => {
             <div className="activity-container">
                 {
                     // volunteers.map(activity => <Activity
+                    // searchResults.map(activity => <Activity
                     searchResults.map(activity => <Activity
                         key ={activity._id}
                         activity ={activity}
